0945c088caf70426d14df2e67a568be650dbf76e
Newville <105016736+NewvilleMedia@users.noreply.github.com>
2023-11-17 14:28:29 -0500

    p
    p

--- src/components/Home/PopularProducts.js
+++ src/components/Home/PopularProducts.js
@@ -1,20 +1,56 @@
-import { StyleSheet, View, Image } from "react-native";
-import React from "react";
+import { StyleSheet, View, Image, Dimensions } from "react-native";
+import React, { useState } from "react";
 import Design from "../../assets/images/BathroomRemodeling.jpeg";
 import { Text } from "@gluestack-ui/themed";
 import { useTheme } from "@react-navigation/native";
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+
+const { width } = Dimensions.get('window');
+
+const data = [
+  { id: '1', source: require('../../assets/images/ArchitecturalDesigns.jpeg') },
+  { id: '2', source: require('../../assets/images/ArchitecturalDrawing.jpeg') },
+  { id: '3', source: require('../../assets/images/BathroomRemodeling.jpeg') },
+];
 
 const PopularProducts = () => {
   const { colors } = useTheme();
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.slide}>
+        {/* You can render your image or video component here */}
+        <Image source={item.source} style={styles.image} />
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <View style={{ alignItems: "center" }}>
-        <Image source={Design} style={styles.image} />
+      <Carousel
+        data={data}
+        renderItem={renderItem}
+        sliderWidth={width -20}
+        itemWidth={width -20}
+        style={styles.image}
+        onSnapToItem={(index) => setActiveSlide(index)}
+      />
+        <Pagination
+        dotsLength={data.length}
+        activeDotIndex={activeSlide}
+        containerStyle={styles.paginationContainer}
+        dotStyle={styles.dotStyle}
+        inactiveDotStyle={styles.inactiveDotStyle}
+        inactiveDotOpacity={0.6}
+        inactiveDotScale={0.8}
+      />
+
       </View>
       <View style={{ margin: 5, marginTop: 5, padding: 5 }}>
         <Text style={[styles.title, { color: "white" }]}>
-          Victorian Villa by Absolute Architecture
+          Victorian Villa Designs
         </Text>
         <Text style={[styles.user, { color: "white" }]}>
           by James Architecture Group
@@ -45,5 +81,18 @@ const styles = StyleSheet.create({
   },
   user: {
     fontWeight: "bold",
+  },
+  paginationContainer: {
+    marginTop: -20,
+    marginBottom: -30
+  },
+  dotStyle: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+  },
+  inactiveDotStyle: {
+    // You can customize the inactive dot style here
   },
 });
--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as Icons from 'react-native-heroicons/solid';
 import HomeScreen from '../screens/Home/HomeScreen'
 import HomeStackNavigator from "./HomeStackNavigator";
+import TestScreen from "../screens/TestScreen";
 
 const BottomTab = createBottomTabNavigator();
 
@@ -26,6 +27,18 @@ const BottomTabNavigator = () => {
           headerShadowVisible: false,
           tabBarIcon: ({color, style}) => (
             <Icons.HomeModernIcon size={30} color={color} />
+          ),
+        }}
+      />
+         <BottomTab.Screen
+        name="Test"
+        component={TestScreen}
+        options={{
+          unmountOnBlur: true,
+          headerShown: false,
+          headerShadowVisible: false,
+          tabBarIcon: ({color, style}) => (
+            <Icons.RocketLaunchIcon size={30} color={color} />
           ),
         }}
       />
