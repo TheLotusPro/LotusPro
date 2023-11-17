98be46835a40904d7dfa63200629c8ad7f81189d
Newville <105016736+NewvilleMedia@users.noreply.github.com>
2023-11-16 22:35:43 -0500

    p
    p

---
+++ src/theming/DarkTheme.tsx
@@ -0,0 +1,15 @@
+import type {Theme} from '../theming/types';
+
+const DarkTheme: Theme = {
+  dark: true,
+  colors: {
+    primary: 'rgb(10, 132, 255)',
+    background: '#101010',
+    card: '#101010',
+    text: 'rgb(229, 229, 231)',
+    border: '#101010',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
+
+export default DarkTheme;
