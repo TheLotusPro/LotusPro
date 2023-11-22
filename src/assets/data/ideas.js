a23a739e79abebf87790a008b655cd988e9ac366
Newville <105016736+NewvilleMedia@users.noreply.github.com>
2023-11-21 21:11:26 -0500

    p
    p

--- src/assets/data/ideas.js
+++ src/assets/data/ideas.js
@@ -1,6 +1,7 @@
 import Kitchen from '../images/HomeDesign&Remodel/KitchenBath.jpeg'
 import Bathroom from '../images/HomeDesign&Remodel/BathroomRemodel.webp'
 
+
 export default [
     {
         id: '1',
@@ -11,5 +12,80 @@ export default [
         id: '2',
         image: Bathroom,
         title: "Bathroom"
-    }
+    },
+    {
+        id: '3',
+        image: Bathroom,
+        title: "Bedroom"
+    },
+    {
+        id: '4',
+        image: Bathroom,
+        title: "Living Room"
+    },
+    {
+        id: '5',
+        image: Bathroom,
+        title: "Dining Room"
+    },
+    {
+        id: '6',
+        image: Bathroom,
+        title: "Outdoors"
+    },
+    {
+        id: '7',
+        image: Bathroom,
+        title: "Kids Room"
+    },
+    {
+        id: '8',
+        image: Bathroom,
+        title: "Home Office"
+    },
+    {
+        id: '9',
+        image: Bathroom,
+        title: "Storage"
+    },
+    {
+        id: '10',
+        image: Bathroom,
+        title: "Exterior"
+    },
+    {
+        id: '11',
+        image: Bathroom,
+        title: "Basement"
+    },
+    {
+        id: '12',
+        image: Bathroom,
+        title: "Entry"
+    },
+    {
+        id: '13',
+        image: Bathroom,
+        title: "Gym"
+    },
+    {
+        id: '14',
+        image: Bathroom,
+        title: "Bar"
+    },
+    {
+        id: '15',
+        image: Bathroom,
+        title: "Hallways"
+    },
+    {
+        id: '16',
+        image: Bathroom,
+        title: "Wine Cellar"
+    },
+    {
+        id: '17',
+        image: Bathroom,
+        title: "Pool Area"
+    },
 ]
