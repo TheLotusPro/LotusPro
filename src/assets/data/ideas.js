fa28b1c290dac13959682bfcade5ff65d52e41af
Newville <105016736+NewvilleMedia@users.noreply.github.com>
2023-11-21 22:17:33 -0500

    P
    P

--- src/assets/data/ideas.js
+++ src/assets/data/ideas.js
@@ -1,6 +1,20 @@
 import Kitchen from '../images/HomeDesign&Remodel/KitchenBath.jpeg'
 import Bathroom from '../images/HomeDesign&Remodel/BathroomRemodel.webp'
-
+import Bedroom from '../images/Ideas/Bedroom.jpg'
+import LivingRoom from '../images/Ideas/LivingRoom.jpg'
+import Dining from '../images/Ideas/DiningRoom.jpg'
+import Outdoors from '../images/Ideas/Outside.jpg'
+import Kids from '../images/Ideas/KidsRoom.jpg'
+import Office from '../images/Ideas/Office.jpg'
+import Storage from '../images/Ideas/Storage.jpg'
+import Exterior from '../images/Ideas/Exterior.jpg'
+import Basement from '../images/Ideas/Basement.jpg'
+import Gym from '../images/Ideas/Gym.jpg'
+import Bar from '../images/Ideas/Bar.jpg'
+import Hallway from '../images/Ideas/Hallway.jpg'
+import Wine from '../images/Ideas/WineCellar.jpg'
+import Pool from '../images/Ideas/PoolArea.jpg'
+import Entry from '../images/Ideas/Entry.jpg'
 
 export default [
     {
@@ -15,77 +29,77 @@ export default [
     },
     {
         id: '3',
-        image: Bathroom,
+        image: Bedroom,
         title: "Bedroom"
     },
     {
         id: '4',
-        image: Bathroom,
+        image: LivingRoom,
         title: "Living Room"
     },
     {
         id: '5',
-        image: Bathroom,
+        image: Dining,
         title: "Dining Room"
     },
     {
         id: '6',
-        image: Bathroom,
+        image: Outdoors,
         title: "Outdoors"
     },
     {
         id: '7',
-        image: Bathroom,
+        image: Kids,
         title: "Kids Room"
     },
     {
         id: '8',
-        image: Bathroom,
+        image: Office,
         title: "Home Office"
     },
     {
         id: '9',
-        image: Bathroom,
+        image: Storage,
         title: "Storage"
     },
     {
         id: '10',
-        image: Bathroom,
+        image: Exterior,
         title: "Exterior"
     },
     {
         id: '11',
-        image: Bathroom,
+        image: Basement,
         title: "Basement"
     },
     {
         id: '12',
-        image: Bathroom,
+        image: Entry,
         title: "Entry"
     },
     {
         id: '13',
-        image: Bathroom,
+        image: Gym,
         title: "Gym"
     },
     {
         id: '14',
-        image: Bathroom,
+        image: Bar,
         title: "Bar"
     },
     {
         id: '15',
-        image: Bathroom,
+        image: Hallway,
         title: "Hallways"
     },
     {
         id: '16',
-        image: Bathroom,
+        image: Wine,
         title: "Wine Cellar"
     },
     {
         id: '17',
-        image: Bathroom,
+        image: Pool,
         title: "Pool Area"
     },
 ]
